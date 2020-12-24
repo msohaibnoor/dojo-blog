937194fbc54e13783a06beae8eeece3a950b34e9
Muhammad Sohaib Noor <sohaibfarrukh59@gmail.com>
2020-12-25 00:58:41 +0500

    Added Navbar and Home components

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/">Home</a>
+      <a href="/blog">Blog</a>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/navbar/home/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-h1">Home</div>
+    </div>
+  );
+};
+
+export default Home;
