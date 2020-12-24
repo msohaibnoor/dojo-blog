89f31d08bb05035083351a0f86ce8c71aaa125d3
Muhammad Sohaib Noor <sohaibfarrukh59@gmail.com>
2020-12-25 01:01:56 +0500

    fixed path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./components/navbar/home/Home";
+import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
